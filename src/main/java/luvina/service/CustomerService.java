7445796c19d3d80499267280478e31d0886e8cca
ღ Snow ♡ ♡ Flower ღ <thanhdai7241@gmail.com>
2018-07-24 10:17:52 +0700

    customer

---
+++ src/main/java/luvina/service/CustomerService.java
@@ -0,0 +1,21 @@
+package luvina.service;
+
+import java.util.List;
+import luvina.model.Customer;
+
+public interface CustomerService {
+    // tim toan bo customer
+    Iterable<Customer> findAll();
+
+    //tim kiem customer theo thuoc tinh
+    List<Customer> search(String q);
+
+    //tìm kiem 1 product_ theo customerId
+    Customer findCust_ID (Integer cust_id);
+
+    //luu customer
+    void save(Customer customer);
+
+    //xoa 1 customer theo Cust_id
+    void deleteByCust_id(Integer cust_id);
+}
